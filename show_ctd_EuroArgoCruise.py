1abf5765bb60110314c419545fb0898af6fafcbf
Siiriä Simo-Matti (FMI) <Simo-Matti.Siiria@fmi.fi>
2021-11-09 18:40:56 +0200

    separated fe functions from plotter to argopy helper script

--- show_ctd_EuroArgoCruise.py
+++ show_ctd_EuroArgoCruise.py
@@ -19,6 +19,7 @@ import matplotlib.dates as mdates
 import math
 import gsw
 import warnings
+import arandapy as apy
 warnings.simplefilter("ignore") #applises some depracating time-axis thing.
 C = "sal00"
 T = "t090C"
@@ -27,9 +28,9 @@ D = "depSM"
 #T = "tnc90C"  # RBR Test
 #D = "prM"  # RBR Test
 #in_dir = "C:\\Users\\siirias\\Documents\\Aranda2020\\CTD_DATAA\\"
-in_dir = "D:\\Data\\EuroArgoCruise\\sbetulos\\"
+in_dir = "C:\\Data\\EuroArgoCruise\\sbetulos\\"
 #in_dir = "D:\\Data\\ArandaVEMIVE2020\\RBRTEST\\"
-out_dir = "D:\\Data\\figures\\Aranda\\EuroARgoCruise\\new_shape\\"
+out_dir = "C:\\Data\\figures\\Aranda\\EuroARgoCruise\\new_shape\\"
 out_dir_add = "\\"
 close_figures_when_saved = True
 colormaps = [
@@ -193,16 +194,10 @@ for plot_set in plot_sets:
         ok_index_list = list(range(start_ind,end_ind+1))
     if(type(plot_set[1])==list): #define list of indices used
         ok_index_list = plot_set[1]
-    in_files_tmp = os.listdir(in_dir)  # all files
-    in_files_tmp = [i for i in in_files_tmp if re.match("a{}.*a\.cnv".format(cruise_year), i)] # right types
-    # then separate with index
-    in_files = []
-    for i in in_files_tmp:
-        index_no = int(re.search(".*(\d\d\d\d)a\.cnv",i).groups()[0])
-#        if( index_no >= start_ind and (end_ind<0 or index_no<=end_ind)):
-        if( index_no in ok_index_list): #pick the ones listed
-            if(index_no not in filtered_indices): #but not in black-listed ones
-                in_files.append(i)
+    in_files = apy.get_aranda_filenames(in_dir, \
+                             whitelist = ok_index_list,\
+                             blacklist = filtered_indices,\
+                             cruise_year = cruise_year)
     number_of_files = len(in_files)
     ctd_datas = []
     file_no = 0
@@ -214,115 +209,11 @@ for plot_set in plot_sets:
     latitude = 60.0
     longitude = 20.0
     for in_file in in_files:
-        lines = open(in_dir+in_file,'r').readlines()
-        end_found = False  #search end to find start of data
-        data = []
-        columns = []
-        long_names = []
-        unit_names = []
-        station_name = None
-        for l in lines:
-            #search for the headers
-            if(re.match("# name \d?",l)):
-                try:
-                    index = re.search("# name (\d?)",l).groups()[0].strip()
-                except:
-                    index = None
-                try:
-                    name = re.search("# name \d?.*=([^:]*)",l).groups()[0].strip()
-                except:
-                    name = None
-                try:
-                    long_name = re.search("# name \d?.*=[^:]*:([^\[]*)",l).groups()[0].strip()
-                except:
-                    long_name = ""
-                try:
-                    unit_name = re.search("# name \d?.*\[(.*)\]",l).groups()[0].strip()
-                except:
-                    unit_name = ""
-                columns.append(name)                  
-                long_names.append(long_name)                  
-                unit_names.append(unit_name)                  
-            # search other than column metadata
-            if(re.match("\*\* Station name",l)):
-                try:
-                    station_name = re.search("\*\* Station name.*:(.*)",l).groups()[0].strip()
-                except:
-                    print("WARNING: station name failed:",l)
-                    station_name = "?"
-                station_names.append(station_name)
-
-            if(re.match("\*\* Index",l)):
-                try:
-                    station_index = int(re.search("\*\* Index.*:(.*)",l).groups()[0].strip())
-                except:
-                    station_index = 0
-                station_indices.append(station_index)
-                
-            
-            if(re.match("\*\* Latitude",l)):
-                try:
-                    latitude = re.search("\*\* Latitude.*:(.*)",l).groups()[0].strip()
-                    latitude = float(re.search("(\d*) \d",latitude).groups()[0]) +\
-                               float(re.search("\d* ([\d\.]*)",latitude).groups()[0])/60.0 
-                except:
-                    latitude = 0.0
-                lats.append(latitude)
-            if(re.match("\*\* Longitude",l)):
-                try:
-                    longitude = re.search("\*\* Longitude.*:(.*)",l).groups()[0].strip()
-                    longitude = float(re.search("(\d*) \d",longitude).groups()[0]) +\
-                               float(re.search("\d* ([\d\.]*)",longitude).groups()[0])/60.0 
-                except:
-                    longitude = 0.0
-                lons.append(longitude)
-
-#            if(re.match("\*\* Date and time",l)):
-#                try:
-#                    tmp_l= l                    
-#                    tmp_l = re.sub(",","",tmp_l) #fix extra comma in header
-#                    tmp_l = re.sub("(\d):(\d)",r"\1.\2",tmp_l) #fix : instead of . in time
-#                    the_time = re.search("\*\*.*:(.*)",tmp_l).groups()[0].strip()
-#                    the_time = dt.datetime.strptime(the_time,"%d.%m.%Y %H.%M")
-#                except:
-#                    print("WARNING, Can't parse time!: {}".format(l))
-#                    the_time = dt.datetime(2000,1,1)
-#                times.append(the_time)
-
-            if(re.match("# start_time",l)):  
-                try:
-                    the_time = re.search(\
-                             "# start_time = ([a-zA-z]* \d* \d* \d*:\d*:\d*)"\
-                             ,l).groups()[0].strip()
-                    #esim: Oct 15 2020 16:43:44
-                    the_time = dt.datetime.strptime(the_time,"%b %d %Y %H:%M:%S")
-                except:
-                    print("WARNING, Can't parse time!: {}".format(l))
-                    the_time = dt.datetime(2000,1,1)
-                times.append(the_time)
-
-            if(end_found):
-                l_t = re.sub("\s\s*"," ",l.strip()).split(" ")
-                l_t = list(map(lambda x: float(x),l_t))
-                l_t.append(latitude)
-                l_t.append(longitude)
-                l_t.append(the_time)
-                data.append(l_t)
-            if(re.match('\*END\*',l)):
-                end_found = True
-        if(not station_name):
-            station_name = "unknown"
-            station_names.append(station_name)
-            
-        columns.append('Lat')
-        columns.append('Lon')
-        columns.append('Time')
-        long_names.append('Latitude')
-        long_names.append('Longitude')
-        long_names.append('Time')
-        ctd_data = pd.DataFrame(data,columns = columns)
-        ctd_datas.append(ctd_data)
-
+        [data, station_index, station_name, columns, long_names, unit_names] = \
+            apy.read_aranda_file(os.path.join(in_dir,in_file))
+        station_indices.append(station_index)
+        station_names.append(station_name)
+        ctd_datas.append(data)
 
     #plot TS-diagrams
     file_no = 0
